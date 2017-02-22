d6e10b8901395966d5ebaaaf882a787ae838d552
ABY ABRAHAM KALATHIVILA <abyabrkal@gmail.com>
2017-02-22 15:11:45 +0400

    wikiapi-start
    wiki api def started

--- js/script.js
+++ js/script.js
@@ -3,6 +3,13 @@
 
 $(document).ready(function () {
     
+    /******* Constructing URL ***** */
+    var baseURL = "http://en.wikipedia.org/w/api.php?";
+    
+    var searchPfix = "?action=query&list=search&format=json&srsearch=";
+    
+    
+    var corsSfix = "&callback=?";
     
     
     // Get data from Wikipedia 
