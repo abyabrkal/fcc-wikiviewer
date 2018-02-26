4565722e944932f357c8c025987afe4057e0c34d
ABY ABRAHAM KALATHIVILA <abyabrkal@gmail.com>
2018-02-26 16:16:49 +0400

    Wiki Ui redesign

--- js/script.js
+++ js/script.js
@@ -1,32 +1,48 @@
-// WIKIPEDIA VIEWER
 
-
-$(document).ready(function () {
-    
-    /**TESTING WATERS**/
-    
-
-    
-    
-    
-    
-    /******* Constructing URL ***** 
-    var baseURL = "http://en.wikipedia.org/w/api.php?";
-    
-    
-    
-    var searchPfix =  "action=query&format=json&prop=info%7Cpageimages&titles=Thor&inprop=displaytitle%7Curl%7Csubjectid"
-    
-    
-    var corsSfix = "&callback=?";
-    
-    
-    // Get data from Wikipedia 
-    function getWikiData() {
-        
-        $.getJSON();
-        
-    }  
-    */
-    
+$('#search').focus(function(){
+	$('#search').val('');
 });
+
+
+$('#search').keypress(function(e) {
+    if(e.which == 13) {
+	   	e.preventDefault(); 
+        loadWiki();
+    }
+});
+
+
+function loadWiki(){
+	
+	var search = $('#search').val(); 
+	//console.log(search);
+	
+	var wikiURL = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&search=" + search + "&namespace=0&limit=10";
+	
+	$.ajax({
+		url: wikiURL,
+		success: function(data){
+			console.log(data);
+			 
+			$('#results').html('');
+			
+			for (var i = 1; i < data[1].length; i++){
+				console.log(data[3][i]);
+				
+				$('#results').append('<li class="list"><div class="left">' + i + '</div><div class="right"><a class="mainhead" href="' + data[3][i] + '" target="_blank">'+ data[1][i] +'</a><p class="brief">' + data[2][i] + '</p></div></li>');
+			}		
+			
+		},
+		error: function(){
+			$('#results').innerHTML("Error retrieving data.");
+		}
+		
+	});
+	
+}
+
+$(document).ready(function() {
+	
+	$('#searchBtn').click(loadWiki);
+	
+ });
